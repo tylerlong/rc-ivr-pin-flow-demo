ccd093467430520fee4a3ae99c6e4e0766e70bae
Tyler Liu <tyler4long@gmail.com>
2026-02-11 10:21:52 -0800

    Fully working version

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,19 @@
 import process from "node:process";
+import RingCentral from "@rc-ex/core";
 
-console.log("hello world!");
-console.log(process.env.RINGCENTRAL_SERVER_URL);
+const rc = new RingCentral({
+  server: process.env.RINGCENTRAL_SERVER_URL,
+  clientId: process.env.RINGCENTRAL_CLIENT_ID,
+  clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET,
+});
+
+const main = async () => {
+  const token = await rc.getToken({
+    grant_type: "ivr_pin",
+    username: process.env.RINGCENTRAL_EXTENSION_MAIN_PHONE_NUMBER,
+    ivr_pin: process.env.RINGCENTRAL_IVR_PIN,
+  });
+  console.log("Yeah, you got the token!");
+  console.log(token.access_token);
+};
+main();
